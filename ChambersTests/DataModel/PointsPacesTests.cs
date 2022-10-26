e338861348ea473e36921c19529ab0eebc1bc9a3
Julio Santos <julioSantos@hotmail.com>
2022-10-26 15:11:53 -0400

    PointsPacesTests renamed and refactored

---
+++ ChambersTests/DataModel/PointsPacesTests.cs
@@ -0,0 +1,34 @@
+﻿using ChambersDataModel;
+using System.Runtime.CompilerServices;
+
+namespace ChambersTests.DataModel
+{
+    [TestClass]
+    public class PointsPaceTests
+    {
+
+        private static string NewName([CallerMemberName] string? name = null) {
+            var newName = nameof(StageDatesTests) + "_" + name;
+            return newName;
+        }
+
+        public static PointsPace NewPointsPace(string stageName) {
+            var tag = TagTests.NewTag(stageName);
+            TestDbContext.Tags.Add(tag);
+            var pointsPace = new PointsPace() { TagId = tag.TagId };
+            return pointsPace;
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            var name = NewName();
+            var pointsPace = NewPointsPace(name);
+            pointsPace.NextStepStartTime = new DateTime(2022, 01, 01);
+            pointsPace.StepSizeDays = 3;
+            TestDbContext.PointsPaces.Add(pointsPace);
+            var savedCount = TestDbContext.SaveChanges();
+            Assert.AreEqual(2, savedCount);
+        }
+    }
+}
