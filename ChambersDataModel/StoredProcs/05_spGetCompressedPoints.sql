03c77cd8336a81de1191dea317d253755d8a32e1
Julio Santos <julioSantos@hotmail.com>
2022-11-02 15:28:28 -0400

    spGetCompressedPoints support for two cycles + respective unit test

--- ChambersDataModel/StoredProcs/05_spGetCompressedPoints.sql
+++ ChambersDataModel/StoredProcs/05_spGetCompressedPoints.sql
@@ -10,8 +10,11 @@
 AS
 BEGIN
 DECLARE @ExcId int = 0, @HiExcType varchar(16) = 'HiExcursion'
-, @RampOutFromHiExcType varchar(16) = 'RampOutFromHi' 
-, @RampInToHiExcType varchar(16) = 'RampInToHi' ;
+, @RampOutFromHiExcType varchar(16) = 'RampOut' 
+, @RampInToHiExcType varchar(16) = 'RampIn' ;
+
+DECLARE @FullExcCycle as TABLE ( ExcId int Not Null, tag varchar(255) NOT NULL, time DateTime NOT NULL
+, value float NOT NULL, excType varchar(16) NOT NULL );
 
 DECLARE @HiExcPoints as TABLE ( ExcId int Not Null, tag varchar(255) NOT NULL, time DateTime NOT NULL
 , value float NOT NULL, excType varchar(16) NOT NULL );
@@ -21,10 +24,12 @@ DECLARE @RampInPoints as TABLE ( ExcId int Not Null,  tag varchar(255) NOT NULL,
 , value float NOT NULL, excType varchar(16) NOT NULL );
 
 DECLARE  @tag varchar(255), @time DateTime, @value float;
+DECLARE @ROexcId int,  @ROtag varchar(255), @ROtime DateTime, @ROvalue float, @ROexcType varchar(16);
 
 	DECLARE CPoint CURSOR
 		FOR SELECT [tag], [time], [value] from  [dbo].[CompressedPoints]
-		WHERE tag = @TagName AND time >= @StartDate AND time < @EndDate AND value > @HiThreashold
+		WHERE tag = @TagName AND time >= @StartDate AND time < @EndDate 
+		AND (value > @HiThreashold OR value < @LowThreshold)
 		ORDER BY time;
 
 		SET @ExcId = @ExcId + 1;
@@ -32,18 +37,38 @@ DECLARE  @tag varchar(255), @time DateTime, @value float;
 		PRINT 'Fetch first Excursion point'
 		FETCH NEXT FROM CPoint INTO @tag, @time, @value;
 
+		PRINT 'Loop through Excursion points in the time period if any'
 		WHILE @@FETCH_STATUS = 0  
 		BEGIN
+
+			PRINT 'IF RampOut exists ..'
 			IF EXISTS (SELECT * FROM @RampOutPoints) BEGIN 
-				PRINT 'RampOut found' 
+				PRINT '..If current Excursion Time greater than RampOut Time (next Excursion cycle) ..'
+				IF (@time > @ROtime) BEGIN
+					PRINT '.. save current full Excursion in @FullExcCycle tmpTbl, and  ..'
+					INSERT INTO @FullExcCycle
+					SELECT * FROM @RampInPoints UNION ALL SELECT * FROM @HiExcPoints UNION SELECT * FROM @RampOutPoints;
+					PRINT '.. prepare for next Excursion by clearing Temp tables and incrementing ExcId'
+					DELETE @HiExcPoints;
+					DELETE @RampInPoints;
+					DELETE @RampOutPoints;
+					SET @ExcId = @ExcId + 1;
+					UPDATE @HiExcPoints Set ExcId = @ExcId;
+				END
 			END
-			ELSE BEGIN
+			
+			PRINT 'Save Excursion point in Temp table'
+			INSERT INTO @HiExcPoints VALUES (@ExcId, @tag, @time, @value, @HiExcType);
+
+			PRINT 'Create Ramp points if they don''t exist for this Excursion cycle'
+			IF NOT EXISTS (SELECT * FROM @RampOutPoints) BEGIN
 				PRINT 'RampIn being created' 
-				INSERT INTO @RampInPoints
+				INSERT INTO @RampInPoints 
 				SELECT TOP 1 @ExcId as 'ExcId', [tag], [time], [value], @RampInToHiExcType as 'ExcType' 
 					from  [dbo].[CompressedPoints]
 				WHERE tag = @tag AND time < @time AND value < @HiThreashold
 				ORDER BY [time] DESC; 
+				--UPDATE @RampInPoints SET ExcId = @ExcId, ExcType = @RampInToHiExcType;
 
 				PRINT 'RampOut being created' 
 				INSERT INTO @RampOutPoints
@@ -51,19 +76,26 @@ DECLARE  @tag varchar(255), @time DateTime, @value float;
 					from  [dbo].[CompressedPoints]
 				WHERE tag = @tag AND time >= @time AND value < @HiThreashold
 				ORDER BY [time] ASC; 
+				PRINT 'Save RampOut point''s time'
+				SELECT Top 1 @ROtime = time from @RampOutPoints;
 			END
-
-			PRINT 'Saving High Excursion point'
-			INSERT INTO @HiExcPoints VALUES (@ExcId, @tag, @time, @value, @HiExcType);
 
 			PRINT 'Fetch next High Excursion point'
 			FETCH NEXT FROM CPoint INTO @tag, @time, @value;  
 		END;
 
-		CLOSE CPoint;
-		DEALLOCATE CPoint;
+		PRINT 'if Excursion point exists ..'
+		IF EXISTS (SELECT TOP 1 * FROM @HiExcPoints) BEGIN
+			PRINT '..save final points as last Full Excursion Cycle'
+			INSERT INTO @FullExcCycle
+			SELECT * FROM @RampInPoints UNION ALL SELECT * FROM @HiExcPoints UNION SELECT * FROM @RampOutPoints;
 
-	--PRINT 'SELECT * FROM @HiExcPoints';
-	SELECT * FROM @RampInPoints UNION ALL SELECT * FROM @HiExcPoints UNION SELECT * FROM @RampOutPoints;
+		END
 
-END;
+	CLOSE CPoint;
+	DEALLOCATE CPoint;
+
+	PRINT 'RETURN ALL Full Excursion Cycles'
+	SELECT * FROM @FullExcCycle;
+
+END
--- ChambersTests/DataModel/spGetCompressedPointsTests.cs
+++ ChambersTests/DataModel/spGetCompressedPointsTests.cs
@@ -2,44 +2,85 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace ChambersTests.DataModel
 {
+    //For step-in debug tests
+    //EXECUTE  [dbo].[spGetCompressedPoints] 'T1', '2022-01-01', '2022-03-31', 100, 200
+
     [TestClass]
     public class SpGetCompressedPointsTests
     {
-        [TestMethod]
-        public async Task GetCompressedPointTest1()
-        {
-            List<spGetCompressedPointsResult> result;
-            try
-            {
-                await TestDbContext.TruncateAsync<CompressedPoint>();
-                await TestDbContext.SaveChangesAsync();
+        private static string NewName([CallerMemberName] string? name = null) {
+            var newName = nameof(SpGetCompressedPointsTests) + "_" + name;
+            return newName;
+        }
 
-                TestDbContext.CompressedPoints.Add(new CompressedPoint() { Tag = "T1", Time = new DateTime(2022, 01, 09), Value = 150 });
-                TestDbContext.CompressedPoints.Add(new CompressedPoint() { Tag = "T1", Time = new DateTime(2022, 01, 20), Value = 170 });
-                TestDbContext.CompressedPoints.Add(new CompressedPoint() { Tag = "T1", Time = new DateTime(2022, 01, 12), Value = 220 });
-                TestDbContext.CompressedPoints.Add(new CompressedPoint() { Tag = "T1", Time = new DateTime(2022, 01, 08), Value = 140 });
-                TestDbContext.CompressedPoints.Add(new CompressedPoint() { Tag = "T1", Time = new DateTime(2022, 01, 21), Value = 160 });
-                TestDbContext.CompressedPoints.Add(new CompressedPoint() { Tag = "T1", Time = new DateTime(2022, 01, 10), Value = 210 });
-                await TestDbContext.SaveChangesAsync();
-                result = await TestDbContext.Procedures.spGetCompressedPointsAsync(
-                    "T1", new DateTime(2022, 01, 01), new DateTime(2022, 01, 30), 100, 200);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+        [TestInitialize]
+        public void InitializeTests()
+        {
+            var dbContext = BootStrap.TestDbContext;
+            dbContext.Truncate<CompressedPoint>();
+            dbContext.SaveChanges();
+
+        }
+
+        [TestMethod]
+        public async Task OutOfOrderTest()
+        {
+            var dbContext = BootStrap.TestDbContext;
+            var tag = NewName();
+
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 09), Value = 150 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 20), Value = 170 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 12), Value = 220 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 08), Value = 140 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 21), Value = 160 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 10), Value = 210 });
+            await dbContext.SaveChangesAsync();
+            var result = await dbContext.Procedures.spGetCompressedPointsAsync(
+                tag, new DateTime(2022, 01, 01), new DateTime(2022, 03, 31), 100, 200);
 ;
             Assert.AreEqual(4, result.Count);
             Assert.IsTrue(result.First().excType.StartsWith("RampIn"));
             Assert.IsTrue(result.Last().excType.StartsWith("RampOut"));
             Assert.AreEqual(2, result.Count(r => r.excType == "HiExcursion") );
+        }
+
+        [TestMethod]
+        public async Task TwoCyclesTest() {
+            var dbContext = BootStrap.TestDbContext;
+            var tag = NewName();
+
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 08), Value = 140 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 09), Value = 150 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 10), Value = 210 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 12), Value = 220 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 20), Value = 170 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 01, 21), Value = 160 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 02, 08), Value = 141 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 02, 09), Value = 151 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 02, 10), Value = 211 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 02, 12), Value = 221 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 02, 20), Value = 171 });
+            dbContext.CompressedPoints.Add(new CompressedPoint() { Tag = tag, Time = new DateTime(2022, 02, 21), Value = 161 });
+            await dbContext.SaveChangesAsync();
+            var result = await dbContext.Procedures.spGetCompressedPointsAsync(
+                tag, new DateTime(2022, 01, 01), new DateTime(2022, 03, 31), 100, 200);
+            ;
+            Assert.AreEqual(8, result.Count);
+            Assert.IsTrue(result.First().excType.StartsWith("RampIn"));
+            Assert.IsTrue(result.Last().excType.StartsWith("RampOut"));
+            Assert.AreEqual(4, result.Count(r => r.excType == "HiExcursion"));
+        }
+
+        [TestCleanup]
+        public void CleanUpBetweenTests() {
+
         }
     } 
 }
