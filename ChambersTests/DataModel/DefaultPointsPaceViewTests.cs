fb8b782c7bc569eaf1dbdd8f61f472327ea298b6
Julio Santos <julioSantos@hotmail.com>
2022-11-08 18:06:36 -0500

    DefaultPointsPaceViewTests synchronized

--- ChambersTests/DataModel/DefaultPointsPaceViewTests.cs
+++ ChambersTests/DataModel/DefaultPointsPaceViewTests.cs
@@ -17,43 +17,57 @@ namespace ChambersTests.DataModel
             return newName;
         }
 
-        private readonly CountdownEvent syncMethods1And2 = new (1);
+        private static readonly Object syncObj = new ();
+
 
         [TestMethod]
-        public void GetOneRowTest()
-        {
-            var name = NewName();
-            var stage = new Stage(name, 20, 200);
-            var stageDate = new StagesDate(stage, new DateTime(2022, 01, 01), new DateTime(2022, 12, 31));
-            stageDate.Stage.StageSetValues(20, 200);
-            TestDbContext.StagesDates.Add(stageDate);
-            var savedCount = TestDbContext.SaveChanges();
-            var viewResults = TestDbContext.DefaultPointsPaces.ToList();
-            Assert.IsNotNull(viewResults);
-            Assert.AreEqual(1, viewResults.Count);
-            syncMethods1And2.Signal(1);
+        public void GetOneRowTest() {
+            
+            Monitor.Enter(syncObj);
+            try {
+                var name = NewName();
+                var stage = new Stage(name, 20, 200);
+                var stageDate = new StagesDate(stage, new DateTime(2022, 01, 01), new DateTime(2022, 12, 31));
+                stageDate.Stage.StageSetValues(20, 200);
+                TestDbContext.StagesDates.Add(stageDate);
+                var savedCount = TestDbContext.SaveChanges();
+                var viewResults = TestDbContext.DefaultPointsPaces.ToList();
+                Assert.IsNotNull(viewResults);
+                Assert.AreEqual(1, viewResults.Count);
+            }
+            catch (Exception e) { Monitor.Exit(syncObj); }
+
         }
 
         [TestMethod]
         public void DoNotGetNewlyInsertedRowTest() {
+            Monitor.Enter(syncObj);
+            try
+            {
+                var name = NewName();
+                var stage = new Stage(name, 20, 200);
+                var stageDate = new StagesDate(stage, new DateTime(2022, 01, 01), new DateTime(2022, 12, 31));
+                stageDate.Stage.StageSetValues(20, 200);
+                TestDbContext.StagesDates.Add(stageDate);
+                var savedCount = TestDbContext.SaveChanges();
+                var newRow = TestDbContext.DefaultPointsPaces.First();
+                // insert row
+                Debug.Assert(newRow.NextStepStartDate != null, "newRow.NextStepStartDate != null");
+                var pointPace = new PointsPace() { NextStepStartDate = (DateTime)newRow.NextStepStartDate, TagId = newRow.TagId };
+                TestDbContext.PointsPaces.Add(pointPace);
+                TestDbContext.SaveChanges();
+                // try again. It should get an empty list
+                var viewResults = TestDbContext.DefaultPointsPaces.ToList();
+                Assert.IsNotNull(viewResults);
+                Assert.AreEqual(0, viewResults.Count);
+            }
+            catch (Exception e) { Monitor.Exit(syncObj); }
 
-            syncMethods1And2.Wait(); // will return when cde count reaches 0
-            var name = NewName();
-            var stage = new Stage(name, 20, 200);
-            var stageDate = new StagesDate(stage, new DateTime(2022, 01, 01), new DateTime(2022, 12, 31));
-            stageDate.Stage.StageSetValues(20, 200);
-            TestDbContext.StagesDates.Add(stageDate);
-            var savedCount = TestDbContext.SaveChanges();
-            var newRow = TestDbContext.DefaultPointsPaces.First();
-            // insert row
-            Debug.Assert(newRow.NextStepStartDate != null, "newRow.NextStepStartDate != null");
-            var pointPace = new PointsPace() { NextStepStartDate = (DateTime)newRow.NextStepStartDate, TagId = newRow.TagId };
-            TestDbContext.PointsPaces.Add(pointPace);
-            TestDbContext.SaveChanges();
-            // try again. It should get an empty list
-            var viewResults = TestDbContext.DefaultPointsPaces.ToList();
-            Assert.IsNotNull(viewResults);
-            Assert.AreEqual(0, viewResults.Count);
+        }
+
+        public void SyncMethods()
+        {
+            
         }
     }
 }
