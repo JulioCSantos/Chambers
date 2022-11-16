09670856b4ee044e0ec36de8a4dd09c640f47268
Julio Santos <julioSantos@hotmail.com>
2022-11-16 08:12:41 -0500

    OSIPI compatibility: fixed conversion of date type to string type for

---
+++ ChambersTests/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿EXEC	[dbo].[spPivotExcursionPoints]
+		@TagName = N'chamber_report_tag_1',
+		@StartDate = N'2022-11-01 08:00:00',
+		@EndDate = N'2022-11-10 08:00:00',
+		@LowThreashold = 100,
+		@HiThreashold = 200
