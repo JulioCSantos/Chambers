5895294861cdce650671eef2153d627ae14b2318
Julio Santos <julioSantos@hotmail.com>
2023-01-29 13:40:39 -0500

    spGetStats proc modified + ExcursionStats table and spGetStats schemas scripts saved

--- ChambersDataModel/StoredProcs/14_spGetStats.sql
+++ ChambersDataModel/StoredProcs/14_spGetStats.sql
@@ -3,27 +3,28 @@ CREATE PROCEDURE dbo.spGetStats
 	@TagName varchar(255) NULL, 
 	@TagExcNbr int NULL 
 AS
-SELECT TagName, TagExcNbr, TagId, Min(Value) as MinValue, max(Value) as MaxValue, Avg(Value) as AverageValue, STDEV(value) as StdDeviation
-From 
-(
-select EP.TagId, EP.TagName, EP.TagExcNbr, EP.FirstExcDate, Stat.tag, Stat.time, Stat.value 
-, ES.TagName as EsTagName, ES.TagExcNbr as EsTagExcNbr
-from [dbo].[ExcursionPoints] as EP
-LEFT JOIN dbo.ExcursionStats as ES
-On EP.TagName = ES.TagName AND EP.TagExcNbr = ES.TagExcNbr
-LEFT JOIN 
- [ELChambers].[dbo].Interpolated as Stat
-ON Stat.tag = EP.TagName and Stat.time >= EP.FirstExcDate And Stat.Time <= EP.LastExcDate
-WHERE 
-(
-@TagName IS NULL OR (
-	EP.TagName = @TagName AND (@TagExcNbr IS NULL OR EP.TagExcNbr = @TagExcNbr)
-	)
-)  
-AND 
-ES.TagName IS null  AND ES.TagExcNbr IS NULL
-) as stats
-Group by TagName, TagExcNbr, TagId
+BEGIN
+DECLARE @EPTagName varchar(255), @EPTagExcNbr int, @EPTagId int,  @EPFirstExcDate DateTime, @EPLastExcDate DateTime
+
+DECLARE EPsCsr CURSOR FOR
+	SELECT EP.TagName, EP.TagExcNbr, EP.TagId, EP.FirstExcDate, EP.LastExcDate FROM [dbo].[ExcursionPoints] as EP
+	LEFT JOIN dbo.ExcursionStats as ES
+	On EP.TagName = ES.TagName AND EP.TagExcNbr = ES.TagExcNbr
+	WHERE @TagName IS NULL OR (EP.TagName = @TagName AND (@TagExcNbr IS NULL OR EP.TagExcNbr = @TagExcNbr))
+	AND ES.TagName IS null  AND ES.TagExcNbr IS NULL
+
+	OPEN EPsCsr;
+	FETCH NEXT FROM EPsCsr INTO @EPTagName, @EPTagExcNbr, @EPTagId, @EPFirstExcDate, @EPLastExcDate;
+	WHILE @@FETCH_STATUS = 0 BEGIN
+		SELECT @EPTagName as TagName, @EPTagExcNbr as TagExcNbr, @EPTagId as TagId, 
+				Min(Value) as MinValue, max(Value) as MaxValue, Avg(Value) as AverageValue, STDEV(value) as StdDeviation
+			FROM [ELChambers].[dbo].Interpolated as Stat
+			WHERE Stat.tag = @EPTagName  and Stat.time >= @EPFirstExcDate And Stat.Time <= @EPLastExcDate
+		FETCH NEXT FROM EPsCsr INTO @EPTagName, @EPTagExcNbr, @EPTagId, @EPFirstExcDate, @EPLastExcDate;
+	END;
+	CLOSE EPsCsr;
+	DEALLOCATE EPsCsr;
 
 --insert into[dbo].[ExcursionStats]
 --EXECUTE dbo.spGetStats chamber_report_tag_1, NULL
+END
