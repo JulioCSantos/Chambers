997368e69a48bf4b807042f8900eb9114aa54a82
Julio Santos <julioSantos@hotmail.com>
2023-03-09 21:47:53 -0500

    StagesLimitsAndDates update to use StagesLimitsAndDatesCore

--- ChambersDataModel/Views/02_StagesLimitsAndDates.sql
+++ ChambersDataModel/Views/02_StagesLimitsAndDates.sql
@@ -1,8 +1,6 @@
 ﻿CREATE VIEW [dbo].[StagesLimitsAndDates]
 AS
-SELECT t.TagId, t.TagName, std.StageDateId, st.StageName, st.MinThreshold, st.MaxThreshold
-, std.StartDate, std.EndDate, st.TimeStep, st.StageId, st.ThresholdDuration, st.SetPoint
-FROM  dbo.Stages AS st INNER JOIN
-         dbo.StagesDates AS std ON st.StageId = std.StageId INNER JOIN
-         dbo.Tags AS t ON st.TagId = t.TagId
-WHERE (std.DeprecatedDate IS NULL) AND (st.DeprecatedDate IS NULL)
+SELECT TagId, TagName, StageDateId, StageName, MinThreshold, MaxThreshold
+, StartDate, EndDate, TimeStep, StageId, ThresholdDuration, SetPoint
+FROM  dbo.[StagesLimitsAndDatesCore]
+WHERE (StageDateDeprecatedDate IS NULL) AND (StageDeprecatedDate IS NULL)
