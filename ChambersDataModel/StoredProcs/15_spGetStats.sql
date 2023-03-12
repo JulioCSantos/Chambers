e35d96dde24449e818226969ddfa0b22d66b39d4
Julio Santos <julioSantos@hotmail.com>
2023-03-12 12:35:27 -0400

    spGetStatsTests updated

--- ChambersDataModel/StoredProcs/15_spGetStats.sql
+++ ChambersDataModel/StoredProcs/15_spGetStats.sql
@@ -10,17 +10,26 @@ AS
 BEGIN
 PRINT '>>> spGetStats begins'
 
-		SELECT @MinValue = Min(Value), @MaxValue = max(Value), @AvergValue = Avg(Value), @StdDevValue = STDEV(value)
-			FROM [dbo].Interpolated as Stat
-			WHERE Stat.tag = @TagName  and Stat.time >= @FirstExcDate And Stat.Time <= @LastExcDate;
 
-PRINT 'spGetStats ends <<<'
+		SELECT @MinValue = Min(Value), @MaxValue = max(Value), @AvergValue = Avg(Value), @StdDevValue = STDEV(value)
+			FROM [dbo].fnGetInterp2(@TagName,@FirstExcDate,@LastExcDate,'00:10:00');
+
+		--SELECT @MinValue = Min(Value), @MaxValue = max(Value), @AvergValue = Avg(Value), @StdDevValue = STDEV(value)
+		--	FROM [dbo].Interpolated as Stat
+		--	WHERE Stat.tag = @TagName  and Stat.time >= @FirstExcDate And Stat.Time <= @LastExcDate;
+
+--PRINT 'spGetStats ends <<<'
 --DECLARE @OMinValue    float;
 --DECLARE @OMaxValue    float;
 --DECLARE @OAvergValue  float;
 --DECLARE @OStdDevValue float;
-----insert into[dbo].[ExcursionStats]
+------insert into[dbo].[ExcursionStats]
 --EXECUTE dbo.spGetStats chamber_report_tag_1, '2022-11-01 12:03:00.00', '2022-11-01 13:57:00.000'
 --	, @MinValue = @OMinValue OUTPUT, @MaxValue = @OMaxValue OUTPUT, @AvergValue = @OAvergValue OUTPUT, @StdDevValue = @OStdDevValue OUTPUT;
---PRINT CONCAT(@OMinValue,'-', @OMaxValue,'-', @OAvergValue,'-', @OStdDevValue);
+--PRINT CONCAT(@OMinValue,' ', @OMaxValue,' ', @OAvergValue,' ', @OStdDevValue);
+
+		--DECLARE @TimeStep time(0);
+		--SELECT @TimeStep = [dbo].fnCalcTimeStep(@FirstExcDate, @LastExcDate, 60);
+		--SELECT @MinValue = Min(Value), @MaxValue = max(Value), @AvergValue = Avg(Value), @StdDevValue = STDEV(value)
+		--	FROM [dbo].fnGetInterp2(@TagName,@FirstExcDate,@LastExcDate, @TimeStep);
 END
--- ChambersTests/DataModel/spGetStatsTests.cs
+++ ChambersTests/DataModel/spGetStatsTests.cs
@@ -21,5 +21,28 @@ namespace ChambersTests.DataModel
             Assert.AreEqual(-1, result);
             Assert.IsNull(minValue.Value);
         }
+
+        [TestMethod]
+        public async Task Retrieving1Test()
+        {
+            var tag = "chamber_report_tag_1";
+            var strDt = new DateTime(2022, 10, 31);
+            var endDt = new DateTime(2022, 11, 30);
+            TestDbContext.CompressedPoints.Add(new CompressedPoint(tag, strDt.AddDays(1), 10));
+            TestDbContext.CompressedPoints.Add(new CompressedPoint(tag, strDt.AddDays(2), 20));
+            TestDbContext.CompressedPoints.Add(new CompressedPoint(tag, strDt.AddDays(3), 30));
+            await TestDbContext.SaveChangesAsync();
+
+            var minValue = new OutputParameter<double?>();
+            var maxValue = new OutputParameter<double?>();
+            var avrgValue = new OutputParameter<double?>();
+            var stdDevValue = new OutputParameter<double?>();
+            var result = await TestDbContext.Procedures.spGetStatsAsync(tag, strDt, endDt
+                , minValue, maxValue, avrgValue, stdDevValue);
+            Assert.AreEqual(10,minValue.Value);
+            Assert.AreEqual(30,maxValue.Value);
+            Assert.AreEqual(20, avrgValue.Value);
+            Assert.IsNotNull(stdDevValue.Value);
+        }
     }
 }
