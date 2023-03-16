d0d96b6eed123c4cecb636a39214196188bcf85d
Julio Santos <julioSantos@hotmail.com>
2023-03-16 11:41:54 -0400

    SMO links include

--- Chambers.Gui/MainWindow.xaml.cs
+++ Chambers.Gui/MainWindow.xaml.cs
@@ -1,21 +1,7 @@
-﻿using Microsoft.Data.SqlClient;
-using Microsoft.SqlServer.Management.Common;
-using Microsoft.SqlServer.Management.Smo;
+﻿using Microsoft.SqlServer.Management.Smo;
 using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Table = Microsoft.SqlServer.Management.Smo.Table;
 
 
