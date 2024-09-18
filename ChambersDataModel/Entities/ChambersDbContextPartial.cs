115c72cc6a8fb92af350aa96fc33996982c7bd2d
JullioSanntos <JulioSantos@Hotmail.com>
2024-09-18 15:28:09 -0400

    new SQL Server name in new lpatop

--- ChambersDataModel/Entities/ChambersDbContextPartial.cs
+++ ChambersDataModel/Entities/ChambersDbContextPartial.cs
@@ -30,10 +30,10 @@ namespace ChambersDataModel.Entities
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             if (!optionsBuilder.IsConfigured) {
                 if (DatabaseName == null) {
-                    optionsBuilder.UseSqlServer("Data Source=ASUS-Strange;Initial Catalog=ELChambers;Integrated Security=True");
+                    optionsBuilder.UseSqlServer("Data Source=ASUS-Strange2;Initial Catalog=ELChambers;Integrated Security=True");
                 }
                 else {
-                    optionsBuilder.UseSqlServer($"Data Source=ASUS-Strange;Initial Catalog={DatabaseName};Integrated Security=True");
+                    optionsBuilder.UseSqlServer($"Data Source=ASUS-Strange2;Initial Catalog={DatabaseName};Integrated Security=True");
                 }
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 //                optionsBuilder.UseSqlServer("Data Source=ASUS-Strange;Initial Catalog=ELChambers;Integrated Security=True");
